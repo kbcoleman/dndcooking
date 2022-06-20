af63cad5138a6429f03ebbbb40f10e70fc41e8f5
katilyn coleman <katilynbreann@gmail.com>
2022-06-20 15:20:02 -0400

    fist commit

---
+++ readme.r
@@ -0,0 +1,6 @@
+This is best viewed in firefox and contains the following features:
+
+    > Conversion tool
+    > Regular Expression
+    > Form w/values saved externally
+    > Read/parse of externa file with display
